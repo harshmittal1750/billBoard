0cf8cb91855fcd64355f8ec0954cd35703e3476d
Harsh Mittal <harshmittal1750@gmail.com>
2022-11-12 23:11:17 +0530

    removed tailwind

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -2,20 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div><ul className="flex border-b">
-            <li className="-mb-px mr-1">
-                <a href="/" className="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold" >Active</a>
-            </li>
-            <li className="mr-1">
-                <a href="/" className="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" >Tab</a>
-            </li>
-            <li className="mr-1">
-                <a href="/" className="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" >Tab</a>
-            </li>
-            <li className="mr-1">
-                <a href="/" className="bg-white inline-block py-2 px-4 text-gray-400 font-semibold" >Tab</a>
-            </li>
-        </ul></div>
+        <div></div>
     )
 }
 
